5ce05172e034e55fbab344aea2fe5f994f57e599
Davide <davide97ls@gmail.com>
2020-07-25 15:01:48 +0800

    Closes #4

--- drl_attacks/strategically_time_attack.py
+++ drl_attacks/strategically_time_attack.py
@@ -1,6 +1,5 @@
 from tianshou.data import Collector
 from advertorch.attacks.base import Attack
-import torch.nn as nn
 import gym
 import time
 import torch
@@ -11,6 +10,7 @@ from tianshou.env import BaseVectorEnv
 from tianshou.policy import BasePolicy
 from tianshou.data import Batch, ReplayBuffer, ListReplayBuffer, to_numpy
 import random as rd
+from tianshou.exploration import BaseNoise
 
 
 class strategically_time_attack_collector(Collector):
@@ -51,9 +51,11 @@ class strategically_time_attack_collector(Collector):
                  buffer: Optional[Union[ReplayBuffer, List[ReplayBuffer]]] = None,
                  preprocess_fn: Callable[[Any], Union[dict, Batch]] = None,
                  stat_size: Optional[int] = 100,
+                 action_noise: Optional[BaseNoise] = None,
+                 reward_metric: Optional[Callable[[np.ndarray], float]] = None,
                  **kwargs
                  ):
-        super().__init__(policy, env, buffer, preprocess_fn, stat_size, **kwargs)
+        super().__init__(policy, env, buffer, preprocess_fn, stat_size, action_noise, reward_metric, **kwargs)
         self.adv = adv  # advertorch attack method
         self.beta = beta
         assert 0 <= beta, \
@@ -89,45 +91,62 @@ class strategically_time_attack_collector(Collector):
             * ``v/ep`` the speed of episode per second.
             * ``rew`` the mean reward over collected episodes.
             * ``len`` the mean length over collected episodes.
-            * ``atk_rate(%)`` ratio of performed attacks over steps.
+            * ``n_attacks`` number of performed attacks.
+            * ``n_succ_atks`` number of performed successful attacks.
+            * ``n_attacks(%)`` ratio of performed attacks over steps.
             * ``succ_atks(%)`` ratio of successful attacks over performed attacks.
         """
-        warning_count = 0  # counts the number of steps
         if not self._multi_env:
             n_episode = np.sum(n_episode)
         start_time = time.time()
         assert sum([(n_step != 0), (n_episode != 0)]) == 1, \
             "One and only one collection number specification is permitted!"
-        cur_step = 0
-        cur_episode = np.zeros(self.env_num) if self._multi_env else 0
-        reward_sum = 0
-        length_sum = 0
+        cur_step, cur_episode = 0, np.zeros(self.env_num)
+        reward_sum, length_sum = 0., 0
+
+        frames_count = 0  # number of observed frames
         n_attacks = 0  # number of attacks performed
         succ_atk = 0  # number of successful image attacks
+
         while True:
-            if warning_count >= 100000:
+            if cur_step >= 100000 and cur_episode.sum() == 0:
                 warnings.warn(
                     'There are already many steps in an episode. '
                     'You should add a time limitation to your environment!',
                     Warning)
-            batch = Batch(
-                obs=self._obs, act=self._act, rew=self._rew,
-                done=self._done, obs_next=None, info=self._info,
-                policy=None)
-            if random:  # take random actions
+
+            # restore the state and the input data
+            last_state = self.data.state
+            if last_state.is_empty():
+                last_state = None
+            self.data.update(state=Batch(), obs_next=Batch(), policy=Batch())
+
+            # calculate the next action
+            if random:
                 action_space = self.env.action_space
                 if isinstance(action_space, list):
                     result = Batch(act=[a.sample() for a in action_space])
                 else:
                     result = Batch(act=self._make_batch(action_space.sample()))
-            else:  # take actions according to a policy
+            else:
                 with torch.no_grad():
-                    result = self.policy(batch, self.state)
-            self.state = result.get('state', None)  # get the state
-            self._policy = to_numpy(result.policy) \
-                if hasattr(result, 'policy') else [{}] * self.env_num
-            self._act = to_numpy(result.act)
-            ##########ADVERSARIAL ATTACK##################
+                    result = self.policy(self.data, last_state)
+
+            # convert None to Batch(), since None is reserved for 0-init
+            state = result.get('state', Batch())
+            if state is None:
+                state = Batch()
+            self.data.state = state
+            if hasattr(result, 'policy'):
+                self.data.policy = to_numpy(result.policy)
+            # save hidden state to policy._state, in order to save into buffer
+            self.data.policy._state = self.data.state
+
+            self.data.act = to_numpy(result.act)
+            if self._action_noise is not None:
+                self.data.act += self._action_noise(self.data.act.shape)
+
+            ##########ADVERSARIAL ATTACK#########
             if self.softmax:
                 softmax = nn.Softmax(dim=1)
                 prob_a = softmax(result.logits).numpy()  # distribution over actions
@@ -137,7 +156,7 @@ class strategically_time_attack_collector(Collector):
             min_a = np.amin(prob_a)
             diff = max_a - min_a
             if diff >= self.beta:
-                ori_act = self._act  # get the original actions
+                ori_act = self.data.act  # get the original actions
                 des_act = np.argmin(prob_a)  # get the desired actions
                 if not self.perfect_attack:
                     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -146,60 +165,46 @@ class strategically_time_attack_collector(Collector):
                     adv_obs = self.adv.perturb(ori_obs, des_act_t)  # create adversarial observations
                     y = self.adv.predict(adv_obs)
                     _, adv_actions = torch.max(y, 1)  # predict adversarial actions
-                    self._act = adv_actions.cpu().detach().numpy()  # replace original actions with adversarial actions
+                    self.data.act = adv_actions.cpu().detach().numpy()  # replace original actions with adversarial actions
                 else:
-                    self._act = [des_act]
-                if self._act == des_act:
+                    self.data.act = [des_act]
+                if self.data.act == des_act:
                     succ_atk += 1
                 n_attacks += 1
-            ###########################################
-            obs_next, self._rew, self._done, self._info = self.env.step(
-                self._act if self._multi_env else self._act[0])  # execute the actions
+            ####################################
+
+            # step in env
+            obs_next, rew, done, info = self.env.step(
+                self.data.act if self._multi_env else self.data.act[0])
+
+            # move data to self.data
             if not self._multi_env:
                 obs_next = self._make_batch(obs_next)
-                self._rew = self._make_batch(self._rew)
-                self._done = self._make_batch(self._done)
-                self._info = self._make_batch(self._info)
+                rew = self._make_batch(rew)
+                done = self._make_batch(done)
+                info = self._make_batch(info)
+            self.data.obs_next = obs_next
+            self.data.rew = rew
+            self.data.done = done
+            self.data.info = info
+
             if log_fn:
-                log_fn(self._info if self._multi_env else self._info[0])
+                log_fn(info if self._multi_env else info[0])
             if render:
-                self.env.render()
+                self.render()
                 if render > 0:
                     time.sleep(render)
+
+            # add data into the buffer
             self.length += 1
-            self.reward += self._rew
+            self.reward += self.data.rew
             if self.preprocess_fn:
-                result = self.preprocess_fn(
-                    obs=self._obs, act=self._act, rew=self._rew,
-                    done=self._done, obs_next=obs_next, info=self._info,
-                    policy=self._policy)
-                self._obs = result.get('obs', self._obs)
-                self._act = result.get('act', self._act)
-                self._rew = result.get('rew', self._rew)
-                self._done = result.get('done', self._done)
-                obs_next = result.get('obs_next', obs_next)
-                self._info = result.get('info', self._info)
-                self._policy = result.get('policy', self._policy)
-            if self._multi_env:
+                result = self.preprocess_fn(**self.data)
+                self.data.update(result)
+            if self._multi_env:  # cache_buffer branch
                 for i in range(self.env_num):
-                    data = {
-                        'obs': self._obs[i], 'act': self._act[i],
-                        'rew': self._rew[i], 'done': self._done[i],
-                        'obs_next': obs_next[i], 'info': self._info[i],
-                        'policy': self._policy[i]}
-                    if self._cached_buf:
-                        warning_count += 1
-                        self._cached_buf[i].add(**data)
-                    elif self._multi_buf:
-                        warning_count += 1
-                        self.buffer[i].add(**data)
-                        cur_step += 1
-                    else:
-                        warning_count += 1
-                        if self.buffer is not None:
-                            self.buffer.add(**data)
-                        cur_step += 1
-                    if self._done[i]:
+                    self._cached_buf[i].add(**self.data[i])
+                    if self.data.done[i]:
                         if n_step != 0 or np.isscalar(n_episode) or \
                                 cur_episode[i] < n_episode[i]:
                             cur_episode[i] += 1
@@ -209,65 +214,74 @@ class strategically_time_attack_collector(Collector):
                                 cur_step += len(self._cached_buf[i])
                                 if self.buffer is not None:
                                     self.buffer.update(self._cached_buf[i])
-                        self.reward[i], self.length[i] = 0, 0
+                        self.reward[i], self.length[i] = 0., 0
                         if self._cached_buf:
                             self._cached_buf[i].reset()
                         self._reset_state(i)
-                if sum(self._done):
-                    obs_next = self.env.reset(np.where(self._done)[0])
+                obs_next = self.data.obs_next
+                if sum(self.data.done):
+                    env_ind = np.where(self.data.done)[0]
+                    obs_reset = self.env.reset(env_ind)
                     if self.preprocess_fn:
-                        obs_next = self.preprocess_fn(obs=obs_next).get(
-                            'obs', obs_next)
+                        obs_next[env_ind] = self.preprocess_fn(
+                            obs=obs_reset).get('obs', obs_reset)
+                    else:
+                        obs_next[env_ind] = obs_reset
+                self.data.obs_next = obs_next
                 if n_episode != 0:
                     if isinstance(n_episode, list) and \
                             (cur_episode >= np.array(n_episode)).all() or \
                             np.isscalar(n_episode) and \
                             cur_episode.sum() >= n_episode:
                         break
-            else:
+            else:  # single buffer, without cache_buffer
                 if self.buffer is not None:
-                    self.buffer.add(
-                        self._obs[0], self._act[0], self._rew[0],
-                        self._done[0], obs_next[0], self._info[0],
-                        self._policy[0])
+                    self.buffer.add(**self.data[0])
                 cur_step += 1
-                if self._done:
+                if self.data.done[0]:
                     cur_episode += 1
                     reward_sum += self.reward[0]
-                    length_sum += self.length
-                    self.reward, self.length = 0, 0
-                    self.state = None
+                    length_sum += self.length[0]
+                    self.reward, self.length = 0., np.zeros(self.env_num)
+                    self.data.state = Batch()
                     obs_next = self._make_batch(self.env.reset())
                     if self.preprocess_fn:
                         obs_next = self.preprocess_fn(obs=obs_next).get(
                             'obs', obs_next)
+                    self.data.obs_next = obs_next
                 if n_episode != 0 and cur_episode >= n_episode:
                     break
             if n_step != 0 and cur_step >= n_step:
                 break
-            self._obs = obs_next
-        self._obs = obs_next
-        if self._multi_env:
-            cur_episode = sum(cur_episode)
+            self.data.obs = self.data.obs_next
+        self.data.obs = self.data.obs_next
+
+        # generate the statistics
+        cur_episode = sum(cur_episode)
         duration = max(time.time() - start_time, 1e-9)
         self.step_speed.add(cur_step / duration)
         self.episode_speed.add(cur_episode / duration)
         self.collect_step += cur_step
         self.collect_episode += cur_episode
         self.collect_time += duration
-        if n_attacks == 0:
-            n_attacks = 1e-9  # avoid 0 division
         if isinstance(n_episode, list):
             n_episode = np.sum(n_episode)
         else:
             n_episode = max(cur_episode, 1)
+        reward_sum /= n_episode
+        if np.asanyarray(reward_sum).size > 1:  # non-scalar reward_sum
+            reward_sum = self._rew_metric(reward_sum)
+        if n_attacks == 0:
+            n_attacks = 1e-9  # avoid 0 division
         return {
             'n/ep': cur_episode,
             'n/st': cur_step,
             'v/st': self.step_speed.get(),
             'v/ep': self.episode_speed.get(),
-            'rew': reward_sum / n_episode,
+            'rew': reward_sum,
             'len': length_sum / n_episode,
+            'n_atks': n_attacks / np.sum(n_episode),
+            'n_succ_atks': succ_atk / np.sum(n_episode),
             'atk_rate(%)': n_attacks / cur_step,
             'succ_atks(%)': succ_atk / n_attacks,
         }
